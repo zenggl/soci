14690b920bd668a6e9d97f89cce850b170e558f9
zenggl <421465786@qq.com>
2024-08-28 10:34:13 +0800

    设置 ORACLE 编译环境路径 增加 session hashcode

--- include/soci/session.h
+++ include/soci/session.h
@@ -8,11 +8,11 @@
 #ifndef SOCI_SESSION_H_INCLUDED
 #define SOCI_SESSION_H_INCLUDED
 
-#include "soci/soci-platform.h"
-#include "soci/once-temp-type.h"
-#include "soci/query_transformation.h"
 #include "soci/connection-parameters.h"
 #include "soci/logger.h"
+#include "soci/once-temp-type.h"
+#include "soci/query_transformation.h"
+#include "soci/soci-platform.h"
 
 // std
 #include <cstddef>
@@ -21,49 +21,43 @@
 #include <sstream>
 #include <string>
 
-namespace soci
-{
+namespace soci {
 class values;
 class backend_factory;
 
-namespace details
-{
+namespace details {
 
 class session_backend;
 class statement_backend;
 class rowid_backend;
 class blob_backend;
 
-} // namespace details
+}  // namespace details
 
 class connection_pool;
 class failover_callback;
 
-class SOCI_DECL session
-{
+class SOCI_DECL session {
 private:
-
-    void set_query_transformation_(std::unique_ptr<details::query_transformation_function>&& qtf);
-
-
+    void set_query_transformation_(std::unique_ptr<details::query_transformation_function> &&qtf);
 
 public:
     session();
-    explicit session(connection_parameters const & parameters);
-    session(backend_factory const & factory, std::string const & connectString);
-    session(std::string const & backendName, std::string const & connectString);
-    explicit session(std::string const & connectString);
-    explicit session(connection_pool & pool);
+    explicit session(connection_parameters const &parameters);
+    session(backend_factory const &factory, std::string const &connectString);
+    session(std::string const &backendName, std::string const &connectString);
+    explicit session(std::string const &connectString);
+    explicit session(connection_pool &pool);
 
     session(session &&other);
     session &operator=(session &&other);
 
     ~session();
 
-    void open(connection_parameters const & parameters);
-    void open(backend_factory const & factory, std::string const & connectString);
-    void open(std::string const & backendName, std::string const & connectString);
-    void open(std::string const & connectString);
+    void open(connection_parameters const &parameters);
+    void open(backend_factory const &factory, std::string const &connectString);
+    void open(std::string const &backendName, std::string const &connectString);
+    void open(std::string const &connectString);
     void close();
     void reconnect();
 
@@ -80,32 +74,32 @@ public:
 
     // even more sugar
     template <typename T>
-    details::once_temp_type operator<<(T const & t) { return once << t; }
+    details::once_temp_type operator<<(T const &t) {
+        return once << t;
+    }
 
-    std::ostringstream & get_query_stream();
+    std::ostringstream &get_query_stream();
     std::string get_query() const;
 
     template <typename T>
-    void set_query_transformation(T callback)
-    {
+    void set_query_transformation(T callback) {
         set_query_transformation_(std::make_unique<details::query_transformation<T>>(callback));
     }
 
     // Support for custom logging of database operations.
 
     // Set the custom logger to use.
-    void set_logger(logger const & logger);
+    void set_logger(logger const &logger);
 
     // Return the currently used logger, by default, this is an instance of a
     // standard SOCI logger.
-    logger const & get_logger() const;
-
+    logger const &get_logger() const;
 
     // support for basic logging (use set_logger() for more control).
-    void set_log_stream(std::ostream * s);
-    std::ostream * get_log_stream() const;
+    void set_log_stream(std::ostream *s);
+    std::ostream *get_log_stream() const;
 
-    void log_query(std::string const & query);
+    void log_query(std::string const &query);
     std::string get_last_query() const;
 
     void set_got_data(bool gotData);
@@ -122,12 +116,12 @@ public:
     // the current backend doesn't support sequences. If you use sequences for
     // automatically generating primary key values, you should use
     // get_last_insert_id() after the insertion in this case.
-    bool get_next_sequence_value(std::string const & sequence, long long & value);
+    bool get_next_sequence_value(std::string const &sequence, long long &value);
 
     // If true is returned, value is filled with the last auto-generated value
     // for this table (although some backends ignore the table argument and
     // return the last value auto-generated in this session).
-    bool get_last_insert_id(std::string const & table, long long & value);
+    bool get_last_insert_id(std::string const &table, long long &value);
 
     // Returns once_temp_type for the internally composed query
     // for the list of tables in the current schema.
@@ -147,21 +141,18 @@ public:
     // it makes sense to bind either std::string for each output field or soci::column_info for the whole row.
     // Note: table_name is a non-const reference to prevent temporary objects,
     // this argument is bound as a regular "use" element.
-    details::prepare_temp_type prepare_column_descriptions(std::string & table_name);
+    details::prepare_temp_type prepare_column_descriptions(std::string &table_name);
 
     // Functions for basic portable DDL statements.
 
-    ddl_type create_table(const std::string & tableName);
-    void drop_table(const std::string & tableName);
-    void truncate_table(const std::string & tableName);
-    ddl_type add_column(const std::string & tableName,
-        const std::string & columnName, db_type dt,
-        int precision = 0, int scale = 0);
-    ddl_type alter_column(const std::string & tableName,
-        const std::string & columnName, db_type dt,
-        int precision = 0, int scale = 0);
-    ddl_type drop_column(const std::string & tableName,
-        const std::string & columnName);
+    ddl_type create_table(const std::string &tableName);
+    void drop_table(const std::string &tableName);
+    void truncate_table(const std::string &tableName);
+    ddl_type add_column(const std::string &tableName, const std::string &columnName, db_type dt, int precision = 0,
+                        int scale = 0);
+    ddl_type alter_column(const std::string &tableName, const std::string &columnName, db_type dt, int precision = 0,
+                          int scale = 0);
+    ddl_type drop_column(const std::string &tableName, const std::string &columnName);
     std::string empty_blob();
     std::string nvl();
 
@@ -177,13 +168,12 @@ public:
     // (notice the leading space).
     std::string get_dummy_from_clause() const;
 
-
     // Sets the failover callback object.
-    void set_failover_callback(failover_callback & callback);
+    void set_failover_callback(failover_callback &callback);
 
     // for diagnostics and advanced users
     // (downcast it to expected back-end session class)
-    details::session_backend * get_backend() { return backEnd_; }
+    details::session_backend *get_backend() { return backEnd_; }
 
     std::string get_backend_name() const;
 
@@ -192,24 +182,21 @@ public:
     // them).
     //
     // Use the overloads taking db_type instead in the new code.
-    ddl_type add_column(const std::string & tableName,
-        const std::string & columnName, data_type dt,
-        int precision = 0, int scale = 0)
-    {
-        return add_column(tableName, columnName, details::to_db_type(dt),
-            precision, scale);
+    ddl_type add_column(const std::string &tableName, const std::string &columnName, data_type dt, int precision = 0,
+                        int scale = 0) {
+        return add_column(tableName, columnName, details::to_db_type(dt), precision, scale);
     }
-    ddl_type alter_column(const std::string & tableName,
-        const std::string & columnName, data_type dt,
-        int precision = 0, int scale = 0)
-    {
-        return alter_column(tableName, columnName, details::to_db_type(dt),
-            precision, scale);
+    ddl_type alter_column(const std::string &tableName, const std::string &columnName, data_type dt, int precision = 0,
+                          int scale = 0) {
+        return alter_column(tableName, columnName, details::to_db_type(dt), precision, scale);
     }
 
-    details::statement_backend * make_statement_backend();
-    details::rowid_backend * make_rowid_backend();
-    details::blob_backend * make_blob_backend();
+    details::statement_backend *make_statement_backend();
+    details::rowid_backend *make_rowid_backend();
+    details::blob_backend *make_blob_backend();
+
+    void set_connstr_hashcode(const std::string hashcode) { connStrHashcode_ = hashcode; }
+    std::string get_connstr_hashcode() const { return connStrHashcode_; }
 
 private:
     SOCI_NOT_COPYABLE(session)
@@ -225,15 +212,18 @@ private:
 
     bool uppercaseColumnNames_;
 
-    details::session_backend * backEnd_;
+    details::session_backend *backEnd_;
 
     bool gotData_;
 
     bool isFromPool_;
     std::size_t poolPosition_;
-    connection_pool * pool_;
+    connection_pool *pool_;
+
+    // 连接字符串的哈希值，用于快速判断是否需要重新连接
+    std::string connStrHashcode_;
 };
 
-} // namespace soci
+}  // namespace soci
 
-#endif // SOCI_SESSION_H_INCLUDED
+#endif  // SOCI_SESSION_H_INCLUDED
